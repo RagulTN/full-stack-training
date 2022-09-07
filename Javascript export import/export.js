b2ae30473dc244e4d912639318c321697ed08d33
ragul.tn <ragul.tn@brimmatech.com>
2022-09-07 14:13:27 +0530

    JS Export import

---
+++ Javascript export import/export.js
@@ -0,0 +1,21 @@
+export let num_set = [1, 2, 3, 4, 5];
+
+  
+
+export default function hello() {
+
+    console.log("Hello Ragul ");
+
+}
+
+  
+
+export class Greeting {
+
+    constructor(name) {
+
+        this.greeting = "Hello " + name ;
+
+    }
+
+}
