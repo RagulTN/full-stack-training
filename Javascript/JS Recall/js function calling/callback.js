e664052e5dbc955aedb206b9a7182e0c819d695f
RagulSid <ragulsiddharth1@gmail.com>
2022-12-17 15:55:09 +0530

    Js Promises added

--- Javascript/JS Recall/js function calling/callback.js
+++ Javascript/JS Recall/js function calling/callback.js
@@ -14,15 +14,6 @@ setTimeout(function() {
 }, 5000)
 
 
-//-------------------------Notes------------------------
-//asynchronous js scenario, dependency between create and payment functions
-//inversion of hell
-// The two problems that we  faced in callbacks are:-
-// 1) Callback Hell: Asynchronous operations in JavaScript can be achieved through callbacks. Whenever there are multiple dependent Asynchronous operations it will result in a lot of nested callbacks. This will cause a 'pyramid of doom' like structure.
-// 2) Inversion of control: When we give the control of callbacks being called to some other API, this may create a lot of issues. That API may be buggy,  may not call our callback and create order as in the above example, may call the payment callback twice etc.
-//callback hell
-//------------------------------------------------------
-
 const cart = ["shoes", "shirts", "pants"]
 
 api.createOrder(cart, function(){
@@ -32,3 +23,13 @@ api.createOrder(cart, function(){
         })
     })
 }) 
+
+
+//-------------------------Notes------------------------
+//asynchronous js scenario, dependency between create and payment functions
+//inversion of hell
+// The two problems that we  faced in callbacks are:-
+// 1) Callback Hell: Asynchronous operations in JavaScript can be achieved through callbacks. Whenever there are multiple dependent Asynchronous operations it will result in a lot of nested callbacks. This will cause a 'pyramid of doom' like structure.
+// 2) Inversion of control: When we give the control of callbacks being called to some other API, this may create a lot of issues. That API may be buggy,  may not call our callback and create order as in the above example, may call the payment callback twice etc.
+//callback hell
+//------------------------------------------------------
