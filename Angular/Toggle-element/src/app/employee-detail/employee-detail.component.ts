dcf0c5e81d6cae145109bc581d8f039c6e5fd3b5
RagulSid <ragulsiddharth1@gmail.com>
2023-02-01 12:06:29 +0530

    HTTP error handling completed

--- Angular/Toggle-element/src/app/employee-detail/employee-detail.component.ts
+++ Angular/Toggle-element/src/app/employee-detail/employee-detail.component.ts
@@ -8,6 +8,8 @@ import { EmployeeService } from '../employee.service';
 })
 export class EmployeeDetailComponent {
   public employees : any[] = [];
+  public errorMsg: any;
+
 
   constructor(private _employeeService : EmployeeService){}
 
@@ -15,9 +17,15 @@ export class EmployeeDetailComponent {
   //   this.employees = this._employeeService.getEmployees();
   // }
 
+  // ngOnInit(){
+  //   this._employeeService.getEmployees()
+  //       .subscribe(data => this.employees = data);
+  // }
+
   ngOnInit(){
     this._employeeService.getEmployees()
-        .subscribe(data => this.employees = data);
+        .subscribe(data => this.employees = data,
+                   error => this.errorMsg = error);
   }
 
 }
--- Angular/Toggle-element/src/app/employee.service.ts
+++ Angular/Toggle-element/src/app/employee.service.ts
@@ -1,18 +1,29 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, observable } from 'rxjs';
 import { IEmployee } from './employee';
+
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeService {
 
-  private _url: string = "/assets/data/employees.json"
+  private _url: string = "/assets/data/employees1.json"
   constructor(private http: HttpClient) { }
 
-  getEmployees(): Observable<IEmployee[]>{
-    return this.http.get<IEmployee[]>(this._url)
+  // getEmployees(): Observable<IEmployee[]>{
+  //   return this.http.get<IEmployee[]>(this._url)
+  // }
+
+  getEmployees(): Observable<IEmployee[]> {
+    return this.http.get<IEmployee[]>(this._url).pipe(catchError(this.erroHandler));
+  }
+
+  erroHandler(error: HttpErrorResponse) {
+    return throwError(error.message || 'server Error');
   }
 } 
 
