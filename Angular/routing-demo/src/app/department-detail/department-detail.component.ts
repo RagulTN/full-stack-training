bdbe6c9e532989f90f0e93cd8e85b69079a0d897
RagulSid <ragulsiddharth1@gmail.com>
2023-02-13 16:14:54 +0530

    paramMap subscribe added

--- Angular/routing-demo/src/app/department-detail/department-detail.component.ts
+++ Angular/routing-demo/src/app/department-detail/department-detail.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router, ParamMap } from '@angular/router';
 
 @Component({
   selector: 'app-department-detail',
@@ -8,10 +8,26 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class DepartmentDetailComponent {
   public departmentId : any;
-  constructor(private route: ActivatedRoute){}
+  constructor(private route: ActivatedRoute, private router: Router){}
 
   ngOnInit(){
-    let id = parseInt(this.route.snapshot.paramMap.get('id') as any);
-    this.departmentId = id;
+    // let id = parseInt(this.route.snapshot.paramMap.get('id') as any);
+    // this.departmentId = id;
+    this.route.paramMap.subscribe((params: ParamMap) =>{
+      let id = parseInt(params.get('id') as any);
+      this.departmentId = id;
+    })
+  }
+
+  goPrevious(){
+    let previousId = this.departmentId - 1;
+    this.router.navigate(['/departments', previousId]);
+  }
+  goNext(){
+    let previousId = this.departmentId + 1;
+    this.router.navigate(['/departments', previousId]);
   }
 }
+
+//you need activatedRoute for components thats being routed
+//you need router for navigate data one to another
