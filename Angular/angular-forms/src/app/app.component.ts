5801784fea7f00d7d1e9a9d3b6b027fae901ba1b
RagulSid <ragulsiddharth1@gmail.com>
2023-02-15 17:11:12 +0530

    form validaton added

--- Angular/angular-forms/src/app/app.component.ts
+++ Angular/angular-forms/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from './user';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +9,19 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'angular-forms';
   topics = ['full-stack', 'front-end', 'back-end'];
+  topicHasError = true;
+
+  validateTopic(value: any){
+    if (value === 'default'){
+      this.topicHasError = true;
+    }
+    else{
+      this.topicHasError = false;
+    }
+  }
+
+  userModel = new User('', 'ragul@gmail1212.com', 123443, '', 'morning', true );  //sending data to view
 }
+
+// #name="ngModel" -> name (template ref varible) is ngmodel for that input Element
+// [class.is-invalid]="name.invalid" -> apply class is-invalid if name (in ngModel) has invalid property
